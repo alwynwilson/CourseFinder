27748ab2443cc6bea8b895574ba39eb988d1066b
alwynwilson <alwynwilson187@gmail.com>
2024-12-05 05:36:37 +0530

    Home page

---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./header.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [navList, setNavList] = useState(false);
+
+  return (
+    <>
+      <header>
+        <div className="container flex">
+          <div className="logo">
+            <h1 className="header">CourseFinder</h1>
+          </div>
+          <div className="nav">
+            <ul className={navList ? "small" : "flex"}>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+              <li>
+                <Link to="/courses">Courses</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="button flex">
+            <button className="btn1">
+              <i className="fa fa-sign-out"></i> Sign In
+            </button>
+          </div>
+
+          <div className="toggle">
+            <button onClick={() => setNavList(!navList)}>
+              {navList ? (
+                <i className="fa fa-times"></i>
+              ) : (
+                <i className="fa fa-bars"></i>
+              )}
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
